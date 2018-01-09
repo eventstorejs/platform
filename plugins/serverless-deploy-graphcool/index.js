d0469d0088b62df3d440a686d8e7f55fc4b98ce9
Jonas Holtorf <Jonas.Holtorf@gmail.com>
2018-01-09 15:18:32 +0100

    feat(deploy): add ability to export env variables

--- plugins/serverless-deploy-graphcool/index.js
+++ plugins/serverless-deploy-graphcool/index.js
@@ -39,9 +39,38 @@ class Graphcool {
       'graphcool:deploy:init': () => this.init(),
       'graphcool:deploy:deploy': () => this.deployGraphcool(),
       'before:package:initialize': () => this.init(),
+      "before:deploy:deploy": () => this.deployGraphcoolIfNotExists(),
       "after:deploy:finalize": () => this.deployGraphcool()
     };
 
+  }
+
+  getTargets() {
+    let ymlNode = yml.load(fs.readFileSync(path.join(this.graphcool.path, '.graphcoolrc'), {
+      encoding: 'utf-8'
+    }))
+    let targets = {}
+    if (ymlNode) {
+      for (let mapping of ymlNode.mappings) {
+        if (mapping.key.value === 'targets') {
+          for (let t of mapping.value.mappings) {
+            targets[t.key.value] = t.value.value
+          }
+          break
+        }
+      }
+    }
+    return targets
+  }
+
+  getTarget() {
+    return this.aws.request('SSM', 'getParameter', {
+        Name: this.graphcool.ssm
+      }, this.stage, this.region)
+      .then((p) => p.Parameter.Value)
+      .catch((e) => {
+        this.serverless.cli.log(`Existing graphcool service not found`)
+      })
   }
 
   init() {
@@ -49,47 +78,57 @@ class Graphcool {
     if (this.serverless.variables.options.stage) {
       this.stage = this.serverless.variables.options.stage;
     }
-    this.graphcool = _.merge({
+    this.graphcool = _.merge({}, {
+      ssm: `/${this.stage}/graphcool/${this.serverless.service.service}`,
+      env: 'GRAPHCOOL_ENDPOINT',
       path: './graphcool',
       name: `${this.stage}-${this.serverless.service.service}`,
       cluster: 'shared-eu-west-1'
     }, this.serverless.service.custom.graphcool)
-    let ymlNode = yml.load(fs.readFileSync(path.join(this.graphcool.path, '.graphcoolrc'), {
-      encoding: 'utf-8'
-    }))
-    let targets = {}
-    for (let mapping of ymlNode.mappings) {
-      if (mapping.key.value === 'targets') {
-        for (let t of mapping.value.mappings) {
-          targets[t.key.value] = t.value.value
-        }
-        break
-      }
+    return this.getTarget()
+      .then((t) => this.target = t)
+      .then(() => this.serverless.service.provider.environment[this.graphcool.env] = this.target)
+
+  }
+
+  deployGraphcoolIfNotExists() {
+    if (!this.target) {
+      this.serverless.cli.log(`Graphcool does not exists yet. Therefore deploying early!`)
+      return this.deployGraphcool();
+    } else {
+      this.serverless.cli.log(`Graphcool exists as ${this.target}`)
+      return Promise.resolve();
     }
-    if (!targets[this.stage]) {
-      throw new Error(`Graphcool Projekt not yet deployed in ${this.stage} and cannot set env variable. use sls graphcool deploy to deploy first`)
-    }
-    this.serverless.service.provider.environment['GRAPHCOOL_ENDPOINT'] = targets[this.stage]
   }
 
   deployGraphcool() {
-    // return this.aws.request('SSM', 'getParameter', {
-    //   Name: `/${this.stage}/eventstore/key`
-    // }, this.stage, this.region)
-    return Promise.resolve({
-        Parameter: {
-          Value: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTEzNDE4NzgsImNsaWVudElkIjoiY2phYXR4anFoMDZuaDAxNTVpM29maXJueiJ9.weD-CKLSy2izpCnETB0JzsthGPJQt1hP99ff4ICqnt0'
+    // return exec('graphcool login')
+    // .then(() => exec(`npm install`, {
+    //   cwd: this.graphcool.path
+    // }))
+    if(this.target) {
+      fs.writeFileSync(path.join(this.graphcool.path, '.graphcoolrc'), `\
+targets:
+  ${this.stage}: ${this.target}
+  default: ${this.stage}`, 'utf-8')
+    }
+    return exec(`${graphcoolCli} deploy --target ${this.stage} --new-service ${this.graphcool.name} --new-service-cluster ${this.graphcool.cluster}`, {
+        cwd: this.graphcool.path
+      })
+      .then(() => {
+        this.serverless.cli.log(`Deployed to graphcool as ${this.graphcool.name}`)
+        if (this.graphcool.ssm) {
+          this.serverless.cli.log(`Uploading endpoint to ssm as ${this.graphcool.ssm}`)
+          return this.aws.request('SSM', 'putParameter', {
+            Overwrite: true,
+            Name: this.graphcool.ssm,
+            Value: this.getTargets()[this.stage],
+            Type: 'String'
+          }, this.stage, this.region)
+        } else {
+          return Promise.resolve();
         }
       })
-      .then(p => p.Parameter.Value)
-      .then((r) => exec(`graphcool login --token ${r}`))
-      // .then(() => exec(`npm install`, {
-      //   cwd: this.graphcool.path
-      // }))
-      .then(() => exec(`${graphcoolCli} deploy --target ${this.stage} --new-service ${this.graphcool.name} --new-service-cluster ${this.graphcool.cluster}`, {
-        cwd: this.graphcool.path
-      }))
-      .then(() => this.serverless.cli.log(`Deployed to graphcool as ${this.graphcool.name}`))
   }
 }
 
