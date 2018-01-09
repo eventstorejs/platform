edb071e5032863bb779de43927dfb8b6feecdbf1
Jonas Holtorf <Jonas.Holtorf@gmail.com>
2018-01-09 15:44:25 +0100

    feat(config): export service config method as instance method also

--- packages/request/src/services/ssm-config.ts
+++ packages/request/src/services/ssm-config.ts
@@ -10,25 +10,25 @@ const log = logger('config')
 @injectable()
 export class SSMConfig extends Config {
 
-  constructor( @multiInject(APP_CONFIG) config: any, @inject(SSM_TOKEN) private ssm: SSM) {
+  constructor ( @multiInject(APP_CONFIG) config: any, @inject(SSM_TOKEN) private ssm: SSM) {
     super(config)
   }
 
   public static async resolveServiceConfig (ssm: SSM, stage?: string, service?: string) {
     let nextToken
-    let path = `/${stage || process.env['STAGE']}/${service || process.env['SERVICE']}`
-    let parameters = {}
+    const path = `/${stage || process.env['STAGE']}/${service || process.env['SERVICE']}`
+    const parameters = {}
     do {
-      let res: SSM.Types.GetParametersByPathResult = await ssm.getParametersByPath({
+      const res: SSM.Types.GetParametersByPathResult = await ssm.getParametersByPath({
         NextToken: nextToken,
         Path: path,
         Recursive: true,
         WithDecryption: true
       }).promise()
       if (res && res.Parameters) {
-        for (let p of res.Parameters) {
+        for (const p of res.Parameters) {
           if (p && p.Name) {
-            let parameterName = p.Name.replace(`${path}/`, '').split('/').join('.')
+            const parameterName = p.Name.replace(`${path}/`, '').split('/').join('.')
             set(parameters, parameterName, p.Value ? p.Value.trim() : p.Value)
           }
         }
@@ -38,7 +38,11 @@ export class SSMConfig extends Config {
     return parameters
   }
 
-  public async resolve<T>(name: string, def: T | undefined = undefined): Promise<T | undefined> {
+  public async resolveServiceConfig (stage?: string, service?: string) {
+    return await SSMConfig.resolveServiceConfig(this.ssm, stage, service)
+  }
+
+  public async resolve<T> (name: string, def: T | undefined = undefined): Promise<T | undefined> {
     let parameter = await super.resolve<T>(name)
     if (parameter) {
       log.debug(`Parameter ${name} received from local config`)
@@ -46,14 +50,14 @@ export class SSMConfig extends Config {
     }
     parameter = def
     log.debug(`Did not found ${name} local. Resolve using ssm`)
-    let stage = await super.resolve<string>('STAGE')
+    const stage = await super.resolve<string>('STAGE')
     if (!stage) {
       throw new Error(`Could not resolve Paramater. Unknown stage`)
     }
-    let paramterName = `/${stage}${name.charAt(0) !== '/' ? '/' : ''}${name}`
+    const paramterName = `/${stage}${name.charAt(0) !== '/' ? '/' : ''}${name}`
     log.debug(`Getting paramter with key: ${paramterName}`)
     try {
-      let p = await this.ssm.getParameter({
+      const p = await this.ssm.getParameter({
         Name: paramterName,
         WithDecryption: true
       }).promise()
