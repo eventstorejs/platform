3280fe77cdca2aa4ec32920b5f6cd275ad59a4fd
Jonas Holtorf <Jonas.Holtorf@gmail.com>
2018-01-18 18:05:25 +0100

    chore(ci): added script to auto resolve by branch

---
+++ packages/ci-tools/flow/resolve-stage.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const resolveStage = () => {
+  return 'test';
+  if(!process.env['CIRCLE_BRANCH']) {
+    console.log(`Could not resolve CIRCLE_BRANCH. Not running on ci?`)
+    throw new Error(`Could not determine branch`)
+  }
+  let feature =  process.env['CIRCLE_BRANCH'].split('/')[1]
+  feature = feature.split('.').join('-')
+  return feature
+}
+
+console.log(resolveStage())
+
+module.exports.resolveStage = resolveStage;
